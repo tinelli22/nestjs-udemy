cf86f84f9c36131d0fceb96ef49aa02b4ffb5a7f
Michael <michaelrtinelli@gmail.com>
2022-10-18 10:15:34 -0300

    add bcrypt to hashe password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -9,4 +15,24 @@ export class AuthService {
     @InjectRepository(User)
     private authRepository: Repository<User>,
   ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto) {
+    const salt = await bcrypt.genSalt();
+    const hashedPass = await bcrypt.hash(authCredentialsDto.password, salt);
+    const user = this.authRepository.create({
+      ...authCredentialsDto,
+      password: hashedPass,
+    });
+
+    try {
+      await this.authRepository.save(user);
+    } catch (err) {
+      //23505 -> duplicate error
+      if (err.code === '23505') {
+        throw new ConflictException('username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
 }
